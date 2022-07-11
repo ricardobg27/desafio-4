08cd59512a00fc7df669fd1570d0cc4c2bc0d61b
Ricardo Borges <ricardoborges00@hotmail.com>
2022-07-11 17:47:22 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,28 @@
+function frase() {
+    console.log('meu nome é ricardo')
+}
+
+function nome(name) {
+    console.log(`Ola ${name}`)
+}
+
+nome("Ricardo")
+
+function dados(filme,musica) {
+    console.log(`Meu filme é ${filme}, minha musica é ${musica}`)
+}
+
+dados('Nemo','Halo')
+
+function dia(dia1,dia2,dia3){
+    console.log()
+}
+
+dia('segunda','terça','quarta')
+
+function numero(i) {
+    return 3*i
+}
+
+numero(5)
+console.log(numero(3*i))
